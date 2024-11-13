a42fb148507b0015d64eee7e7cd647216ffaa732
jeonDev <jjhwhdgus0727@gmail.com>
2024-11-13 21:07:06 +0900

    Initial commit

---
+++ src/test/kotlin/com/pay/core/PayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pay.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
