2fed2ce5930978efd2c155271aa50d0d9d8224b2
jeonDev <79624332+jeonDev@users.noreply.github.com>
2024-11-20 14:29:14 +0900

    feat : 쿠폰 lock 추가

--- src/main/kotlin/com/pay/core/domain/coupon/repository/CouponRepository.kt
+++ src/main/kotlin/com/pay/core/domain/coupon/repository/CouponRepository.kt
@@ -1,9 +1,13 @@
 package com.pay.core.domain.coupon.repository
 
 import com.pay.core.domain.type.CouponType
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import java.util.Optional
 
 interface CouponRepository:JpaRepository<Coupon, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findByCouponType(couponType: CouponType):Optional<Coupon>
 }
